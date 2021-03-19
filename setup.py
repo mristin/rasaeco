d48c7641c10d30c6339addd445504b5a2861981d
Marko Ristin <marko@ristin.ch>
2021-03-19 17:40:52 +0100

    Release 0.0.14 (#51)
    * Add levels `person` and `crew` (#50)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as fid:
 
 setup(
     name="rasaeco",
-    version="0.0.13",
+    version="0.0.14",
     description="Analyze software requirements in AECO industry",
     long_description=long_description,
     url="https://github.com/mristin/rasaeco",
