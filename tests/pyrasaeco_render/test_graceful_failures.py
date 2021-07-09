ae46e03075973c0d246e80299a42d5197cd72ab6
Marko Ristin <marko.ristin@gmail.com>
2021-07-09 11:58:07 +0200

    Improve error messages on `scenarios_dir`
    This patch makes the error messages a bit more informative when the
    supplied `scenarios_dir` is invalid (*e.g.*, does not exist or not a
    directory).

--- tests/pyrasaeco_render/test_graceful_failures.py
+++ tests/pyrasaeco_render/test_graceful_failures.py
@@ -8,6 +8,49 @@ import unittest
 import rasaeco.pyrasaeco_render
 
 
+class TestInvalidScenariosDir(unittest.TestCase):
+    def test_not_exists(self) -> None:
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            scenarios_dir = os.path.join(tmp_dir, "does-not-exist")
+            argv = ["once", "--scenarios_dir", scenarios_dir]
+
+            stdout = io.StringIO()
+            stderr = io.StringIO()
+
+            exit_code = rasaeco.pyrasaeco_render.run(
+                argv=argv, stdout=stdout, stderr=stderr
+            )
+
+            self.assertEqual(1, exit_code)
+            self.assertEqual(
+                f"The directory you specified in --scenarios_dir does not exist "
+                f"on your system: {scenarios_dir}\n",
+                stderr.getvalue(),
+            )
+
+    def test_not_a_directory(self) -> None:
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            scenarios_dir = os.path.join(tmp_dir, "actually-a-file")
+            with open(scenarios_dir, "wt") as fid:
+                fid.write("oi")
+
+            argv = ["once", "--scenarios_dir", scenarios_dir]
+
+            stdout = io.StringIO()
+            stderr = io.StringIO()
+
+            exit_code = rasaeco.pyrasaeco_render.run(
+                argv=argv, stdout=stdout, stderr=stderr
+            )
+
+            self.assertEqual(1, exit_code)
+            self.assertEqual(
+                f"The path you specified in --scenarios_dir is expected "
+                f"to be a directory, but it is not: {scenarios_dir}\n",
+                stderr.getvalue(),
+            )
+
+
 class TestOnFailureCases(unittest.TestCase):
     def test_that_failures_are_handled_gracefully(self) -> None:
         this_dir = pathlib.Path(os.path.realpath(__file__)).parent
